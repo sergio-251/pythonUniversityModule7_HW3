f51860794e306bb97ec814abacc197af8850af52
sergio-251 <50610151+sergio-251@users.noreply.github.com>
2024-12-05 15:21:30 +0300

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -12,15 +12,18 @@ class WordsFinder:
         return all_words
 
     def find(self, word):
-        return {name: words.index(word.lower()) + 1 for name, words in self.get_all_words().items() if word.lower() in words}
+        return {name: words.index(word.lower()) + 1 for name, words in self.get_all_words().items() if
+                word.lower() in words}
 
     def count(self, word):
         return {name: words.count(word.lower()) for name, words in self.get_all_words().items()}
 
-finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
-                      'Rudyard Kipling - If.txt',
-                      'Mother Goose - Monday’s Child.txt')
-print(finder1.get_all_words())
-print(finder1.find('the'))
-print(finder1.count('the'))
+
+if __name__ == '__main__':
+    finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                          'Rudyard Kipling - If.txt',
+                          'Mother Goose - Monday’s Child.txt')
+    print(finder1.get_all_words())
+    print(finder1.find('the'))
+    print(finder1.count('the'))
 
